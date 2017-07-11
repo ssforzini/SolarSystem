aee83b41e9ed8dcd409515b296ff3712c3dc70cc
ssforzini <sebasforzini@hotmail.com>
2017-07-11 17:27:24 -0300

    Commit del sistema solar, faltan algunas texturas de efectos

---
+++ SolarSystem/Assets/Scripts/TranslationTierra.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TranslationTierra : MonoBehaviour {
+
+	public float velocidad;
+	public Transform pivot;
+
+	void Update () {
+		transform.RotateAround (pivot.position, Vector3.up, velocidad);
+	}
+}
